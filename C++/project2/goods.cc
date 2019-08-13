305d8430442bad4d34329a96acaac1b94b3f6f81
samonim89 <38676716+samonim89@users.noreply.github.com>
2019-08-13 15:37:19 +0900

    Add "const" at in/out parameter as per the review

--- C++/project2/goods.cc
+++ C++/project2/goods.cc
@@ -1,17 +1,17 @@
 #include "./goods.h"
 
 // Set member variable of goods with given index and price values
-void Goods::SetGoodsInfo(int index, int price) {
+void Goods::SetGoodsInfo(const int index, const int price) {
   goods_index_ = index;
   goods_price_ = price;
 }
 
 // Return index information
-int Goods::GetIndex() {
+const int Goods::GetIndex() {
   return goods_index_;
 }
 
 // Return price informaiton
-int Goods::GetPrice() {
+const int Goods::GetPrice() {
   return goods_price_;
 }
